9efdf74c37c2eb0dcdc815861db5b5c8d0bbffda
tpetchel <tpetchel@gmail.com>
2016-09-29 10:57:19 -0400

    Initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: learn_chef_apache2
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+apt_update 'Update the apt cache daily' do
+  frequency 86_400
+  action :periodic
+end
+
+package 'apache2'
+
+service 'apache2' do
+  supports :status => true
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
